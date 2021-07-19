8b24c4a96e3f7162e5a62116296204841ddfb9a3
Vihaan Verma <vverma270705@gmail.com>
2021-07-19 11:01:42 +0530

    Added Speed Counter

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -13,13 +13,48 @@ const Home = () => {
     dotRef1 = useRef(null),
     dotRef2 = useRef(null),
     dotRef3 = useRef(null),
-    dotRef4 = useRef(null);
+    dotRef4 = useRef(null),
+    counterRef1 = useRef(null),
+    counterRef2 = useRef(null),
+    counterRef3 = useRef(null);
+  const counters = [counterRef1, counterRef2, counterRef3];
+  const speed = 500; // The lower the faster
+
   const slides = [slideRef1, slideRef2, slideRef3, slideRef4];
   const dots = [dotRef1, dotRef2, dotRef3, dotRef4];
   let slideIndex = 1;
   useEffect(() => {
     // console.log(slides, dots);
     showSlides(slideIndex);
+    counters.forEach((counter) => {
+      const updateCount = () => {
+        const target = parseInt(
+          counter.current.getAttribute("data-target"),
+          10
+        );
+        const count = parseInt(counter.current.innerText, 10);
+
+        // Lower inc to slow and higher to slow
+        const inc = target / speed;
+
+        // console.log(inc);
+        // console.log(count);
+
+        // Check if target is reached
+
+        if (count < target) {
+          // Add inc to count and output in counter
+          counter.current.innerText = count + inc;
+          // Call function every ms
+          // updateCount();
+          setTimeout(updateCount, 1);
+        } else {
+          counter.current.innerText = target;
+        }
+      };
+
+      updateCount();
+    });
     // const id = setInterval(() => plusSlides(1), 4000);
     // return () => {
     //   clearInterval(id);
@@ -52,9 +87,11 @@ const Home = () => {
     dots[slideIndex - 1].current.classList.add("active");
   }
   return (
-    <section id="home_page" style={{ padding: "1rem" }}>
+    <section id="home_page">
       <section id="bg-image"></section>
-      <section style={{ height: "100vh", position: "relative" }}>
+      <section
+        style={{ height: "100vh", position: "relative", padding: "1rem" }}
+      >
         <section class="para">
           <span class="header">Atheneum</span>
           <br />
@@ -69,7 +106,7 @@ const Home = () => {
           </span>
         </section>
       </section>
-      <section class="slideshow-container">
+      <section class="slideshow-container" style={{ padding: "1rem" }}>
         <section ref={slideRef1} class="mySlides fade">
           <section class="numbertext">1 / 4</section>
           <img src={Picture1} style={{ width: "100%", height: "100%" }} />
@@ -120,6 +157,32 @@ const Home = () => {
             class="dot"
             onClick={() => currentSlide(4)}
           ></span>
+        </section>
+      </section>
+      <section class="counters">
+        <section>
+          <i class="fa fa-tree fa-4x"></i>
+          <section ref={counterRef1} class="counter" data-target="7000000000">
+            0
+          </section>
+          <h3>Trees Cut</h3>
+          <h4>7 Billion</h4>
+        </section>
+        <section>
+          <i class="fa fa-tint fa-4x"></i>
+          <section ref={counterRef2} class="counter" data-target="125000000000">
+            0
+          </section>
+          <h3>Water Wasted</h3>
+          <h4>125 Billion</h4>
+        </section>
+        <section>
+          <i class="fa fa-book-dead fa-4x"></i>
+          <section ref={counterRef3} class="counter" data-target="55000000">
+            0
+          </section>
+          <h3>Books Wasted</h3>
+          <h4>55 Million</h4>
         </section>
       </section>
     </section>
